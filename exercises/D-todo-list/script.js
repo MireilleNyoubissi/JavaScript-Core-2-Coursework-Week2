748aa9e17c65fd0788ae4ebf39c0f35f8f52cb7e
MireilleNyoubissi <nmireillemat@yahoo.fr>
2021-10-02 20:42:09 +0100

    updated exercise D

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,63 +1,36 @@
-// let divElement = document.getElementById("content");
-// let uList = document.createElement("ul");
-
-
-// function todoList(todos) {
-//   // Write your code here...
-//   todos.forEach(item => {
-//     let list = document.createElement("li");
-//     uList.appendChild(list);
-
-//     list.innerHTML = item.todo; 
-//     list.addEventListener("click", modifiedText);
-
-//   })
- 
-//   divElement.appendChild(uList); 
-
-// }
-
-// function modifiedText(){
-//  
-    if ((list.style.textDecoration = "line-through")) {
-      list.addEventListener("click", removeLineThrough);
-    } else {
-      list.addEventListener("click", lineThrough);
-    }
-// }
-
-// function removeText() {
-//   this.style.textDecoration = "none";
-// }
-
-
 function todoList(todos) {
+  
   let content = document.getElementById("content");
+  
   let ul = document.createElement("ul");
   content.appendChild(ul);
+  
   todos.forEach((element) => {
+    
     let li = document.createElement("li");
     let text = document.createTextNode(element.todo);
     
+    li.appendChild(text);
+    ul.appendChild(li);
+    
     li.addEventListener("click", check);
-    function removelinethrough() {
+    
+    function removeLineThrough() {
       li.style.textDecoration = "none";
     }
-    function linethrough() {
+    
+    function lineThrough() {
       li.style.textDecoration = "line-through";
     }
     
     function check() {
       if ((li.style.textDecoration = "line-through")) {
-        li.addEventListener("click", removelinethrough);
+        li.addEventListener("click", removeLineThrough);
       } else {
-        li.addEventListener("click", linethrough);
+        li.addEventListener("click", lineThrough);
       }
-    }
-    li.appendChild(text);
-    ul.appendChild(li);
-  });
-  content.appendChild(ul);
+    }     
+  });  
 }
 
 const todos = [
